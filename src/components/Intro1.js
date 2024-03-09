824c9be2b5fc87559cd8948dd725cbb1f80265a9
John Roberts <bigjlr2004@yahoo.com>
2024-03-09 10:23:25 -0600

    Completed Customizing Components Section

---
+++ src/components/Intro1.js
@@ -0,0 +1,16 @@
+function Intro1() {
+  return (
+    <div className="blog-post-intro">
+      <h2>I've become quite the React Developer</h2>
+      <div>
+        <p>
+          I've completed the React Basics course and I am happy to announce that
+          I'm now a Junior React Developer!
+        </p>
+        <p className="link">Read more...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Intro1;
